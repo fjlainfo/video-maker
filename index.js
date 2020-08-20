f390df9df016926e2e8d776df966d24efda1d735
Francisco Jose de Lima Alves (4134-7589) <francisco.alves@vr.com.br>
2020-08-20 03:03:20 -0300

    feat: add inut robot and state robot

--- index.js
+++ index.js
@@ -1,31 +1,17 @@
 const readline = require('readline-sync')
+const robot = require('./robots/text.js')
 const robots = {
-    text: require('./robots/text.js')
+    input: require('./robots/input.js'),
+    text: require('./robots/text.js'),
+    state: require('./robots/state.js')
 }
 
 async function start() {
-    const content = {
-        maximumSentences: 7
-    }
+    robots.input()
+    await robots.text()
 
-    content.searchTerm = askAndReturnSearchTerm()   
-    content.prefix = askAndReturnPrefix()
-
-    await robots.text(content)
-
-    function askAndReturnSearchTerm() {
-        return readline.question('Type a Wikipedia search term: ')
-    }
-
-    function askAndReturnPrefix() {
-        const prefixes = ['who is','What is', 'The history of']
-        const selectedPrefixIndex = readline.keyInSelect(prefixes)
-        const selectedPrefixText = prefixes[selectedPrefixIndex]
-
-        return selectedPrefixText
-    }
-
-    console.log(JSON.stringify(content, null, 4))
+    const content = robots.state.load()
+    console.dir(content, {depth: null})
 }
 
 start()
