1c154c10611a04ebffde3fdaf7baf916c17a95a3
Francisco Jose de Lima Alves (4134-7589) <francisco.alves@vr.com.br>
2020-08-28 00:55:08 -0300

    feat: create video robot

--- robots/image.js
+++ robots/image.js
@@ -1,4 +1,5 @@
 const imageDownloader = require('image-downloader')
+const gm = require('gm').subClass({imageMagick: true})
 const google = require('googleapis').google
 const custonSearch = google.customsearch('v1')
 const state = require('./state.js')
@@ -11,8 +12,11 @@ async function robot() {
     
     await fetchImagesOfAllSentences(content)
     await downloadAllImages(content)
-
-    //state.save(content)
+    await convertAllImages(content)
+    await creatAllSentenceImages(content)
+    await creatYouTubeThumbnail()
+    
+    state.save(content)
 
     async function fetchImagesOfAllSentences(content){
         for (const sentence of content.sentences) {
@@ -67,6 +71,123 @@ async function robot() {
         return imageDownloader.image({
             url: url,
             dest: `./content/${fileName}`
+        })
+    }
+
+    async function convertAllImages(content) {
+        for (let sentenceIndex = 0; sentenceIndex < content.sentences.length; sentenceIndex++) {
+            await convertImage(sentenceIndex)
+        }
+
+        async function convertImage(sentenceIndex) {
+            return new Promise((resolve, reject) => {
+                const inputFIle = `./content/${sentenceIndex}-original.png[0]`
+                const outputFIle = `./content/${sentenceIndex}-converted.png`
+                const width = 1920
+                const height = 1080
+
+                gm()
+                    .in(inputFIle)
+                        .out('(')
+                            .out('-clone')
+                            .out('0')
+                            .out('-background', 'white')
+                            .out('-blur', '0x9')
+                            .out('-resize', `${width}x${height}^`)
+                        .out(')')
+                        .out('(')
+                            .out('-clone')
+                            .out('0')
+                            .out('-background', 'white')
+                            .out('-resize', `${width}x${height}`)
+                        .out(')')
+                        .out('-delete', '0')
+                        .out('-gravity', 'center')
+                        .out('-compose', 'over')
+                        .out('-composite', )
+                        .out('-extent', `${width}X${height}`)
+                        .write(outputFIle, (error) => {
+                            if (error) {
+                                return reject(error)
+                            }
+                            console.log(`> image converted: ${inputFIle}`)
+                            resolve()
+                        })
+            })
+        }
+    }
+
+    async function creatAllSentenceImages(content) {
+        for (let sentenceIndex = 0; sentenceIndex < content.sentences.length; sentenceIndex++) {
+            await creatSentenceImages(sentenceIndex, content.sentences[sentenceIndex].text)
+        }
+
+        async function creatSentenceImages (sentenceIndex, sentenceText) {
+            return new Promise ((resolve, reject) => {
+                const outPutFile = `./content/${sentenceIndex}-sentence.png`
+
+                const tempaletSettings = {
+                    0: {
+                        size: '1920x400',
+                        gravity: 'center'
+                    },
+                    1: {
+                        size: '1920x1080',
+                        gravity: 'center'
+                    },
+                    2: {
+                        size: '800x400',
+                        gravity: 'west'
+                    },
+                    3: {
+                        size: '1920x400',
+                        gravity: 'center'
+                    },
+                    4: {
+                        size: '1920x1080',
+                        gravity: 'center'
+                    },
+                    5: {
+                        size: '800x400',
+                        gravity: 'west'
+                    },
+                    6: {
+                        size: '1920x400',
+                        gravity: 'center'
+                    }
+                }
+
+                gm()
+                    .out('-size', tempaletSettings[sentenceIndex].size)
+                    .out('-gravity', tempaletSettings[sentenceIndex].gravity)
+                    .out('-background', 'transparent')
+                    .out('-fill', 'white')
+                    .out('-kerning', '-1')
+                    .out(`caption:${sentenceText}`)
+                    .write(outPutFile, (error) =>{
+                        if (error) {
+                            return reject(error)
+                        }
+
+                        console.log(`> sentence created: ${outPutFile}`)
+                        resolve()
+                    })
+            })
+        }
+    }
+
+    async function creatYouTubeThumbnail() {
+        return new Promise ((resolve, reject) => {
+            gm()
+                .in('./content/0-converted.png')
+                .write('./content/youtube-thumbnail.jpg', (error) => {
+                    if(error) {
+                        return reject(error)
+                    }
+
+                    console.log('> Createing YouTube Thumbnail')
+                    resolve()
+                })
         })
     }
 }
